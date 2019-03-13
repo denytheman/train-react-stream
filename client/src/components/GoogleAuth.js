4223b73bc98b1610309d84474fb365ef5909a109
Daniel Kovac <dkovac@logitech.com>
2019-03-12 23:06:56 -0700

    Checking the signed in state

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null }
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '600824778402-m5ppkpfk4bh4ir4g4jm8nvmnqof6vf18.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
             });
         });
     }
 
+    renderAuthButton() {
+        if(this.state.isSignedIn === null) {
+            return <div>Don't know</div>;
+        } else if (this.state.isSignedIn ) {
+            return <div>Signed in</div>
+        } else {
+            return <div>Not signed in</div>
+        }
+    }
+
     render() {
         return (
-            <div>GoogleAuth</div>
+            <div>{this.renderAuthButton()}</div>
         )
     }
 }
