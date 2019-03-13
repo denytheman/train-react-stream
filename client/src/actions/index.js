e23b8da8cc7d2a0b3159d2c48e96bdb645bfeeee
Daniel Kovac <dkovac@logitech.com>
2019-03-13 14:22:12 -0700

    Added all the action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -21,3 +21,35 @@ export const createStream = (formValues) => {
         dispatch({type: actionTypes.CREATE_STREAM, payload: response.data});
     }
 }
+
+export const fetchStreams = () => {
+    return async (dispatch) => {
+        const response = await streams.get('/streams');
+
+        dispatch({type: actionTypes.FETCH_STREAMS, payload: response.data});
+    }
+}
+
+export const fetchStream = (streamId) => {
+    return async (dispatch) => {
+        const response = await streams.get(`/streams/${streamId}`);
+
+        dispatch({type: actionTypes.FETCH_STREAM, payload: response.data});
+    }
+}
+
+export const editStream = (streamId, formValues) => {
+    return async (dispatch) => {
+        const response = await streams.put(`/streams/${streamId}`, formValues);
+
+        dispatch({type: actionTypes.EDIT_STREAM, payload: response.data});
+    }
+}
+
+export const deleteStream = (streamId) => {
+    return async (dispatch) => {
+        await streams.delete(`/streams/${streamId}`);
+
+        dispatch({type: actionTypes.DELETE_STREAM, payload: streamId});
+    }
+}
