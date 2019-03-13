4ad1c27fdc287a6d4d78c5091c6c9147b21bb3a0
Daniel Kovac <dkovac@logitech.com>
2019-03-12 21:58:40 -0700

    Added routing support

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+    return <div>Page One</div>
+}
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App;
