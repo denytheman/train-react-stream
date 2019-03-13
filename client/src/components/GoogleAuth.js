eae365ea9afcf9e6f2eefcbb200b8a6235aca982
Daniel Kovac <dkovac@logitech.com>
2019-03-12 23:13:37 -0700

    Google sign in and sign out

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -11,17 +11,40 @@ class GoogleAuth extends React.Component {
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     }
 
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    }
+
+    onSignIn = () => {
+        this.auth.signIn();
+    }
+
+    onSignOut = () => {
+        this.auth.signOut();
+    }
+
     renderAuthButton() {
-        if(this.state.isSignedIn === null) {
-            return <div>Don't know</div>;
-        } else if (this.state.isSignedIn ) {
-            return <div>Signed in</div>
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else if (this.state.isSignedIn) {
+            return (
+                <button className="ui red google button" onClick={this.onSignOut}>
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            )
         } else {
-            return <div>Not signed in</div>
+            return (
+                <button className="ui red google button" onClick={this.onSignIn}>
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            )
         }
     }
 
